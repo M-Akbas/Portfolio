1d70f81ea3d8d9c3a5948e3187b3bebea47ea424
Muhammed Akbas <md.akbas@gmx.de>
2023-10-26 00:16:24 +0200

    farbe fehler

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
+  
 })
 export class PortfolioComponent {
+  showContent: boolean[] = [false, false, false, false];
 
+  show(index: number) {
+    this.showContent[index] = true;
+  }
+
+  hide(index: number) {
+    this.showContent[index] = false;
+  }
 }
+
+
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
+  isHovered = false; // Hier wird isHovered deklariert
+  icons = [
+    { number: 1 , isHovered: false},
+    { number: 2 , isHovered: false},
+  ]
+  hover(itemNumber: number) {
+    const hoveredItem = this.icons.find(item => item.number === itemNumber); // object 
+    if (hoveredItem) {
+      hoveredItem.isHovered = true;
+    }
 
+  }
+  unhover(itemNumber: number) {
+    const hoveredItem = this.icons.find(item => item.number === itemNumber);
+
+    if (hoveredItem) {
+      hoveredItem.isHovered = false;
+    }
+
+  }
 }
+

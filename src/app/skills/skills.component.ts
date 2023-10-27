870c024d5506ea6b84725b993f483aa1e15f4d4d
Muhammed Akbas <md.akbas@gmx.de>
2023-10-27 21:07:52 +0200

    style

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,15 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
-  isHovered = false; // Hier wird isHovered deklariert
   icons = [
     { number: 1 , isHovered: false},
     { number: 2 , isHovered: false},
+    { number: 3 , isHovered: false},
+    { number: 4 , isHovered: false},
+    { number: 5 , isHovered: false},
+    { number: 6 , isHovered: false},
+    { number: 7 , isHovered: false},
+    { number: 8 , isHovered: false},
+    { number: 9 , isHovered: false},
+    { number: 10 , isHovered: false},
   ]
+  
+  isHovered = false;
+
   hover(itemNumber: number) {
     const hoveredItem = this.icons.find(item => item.number === itemNumber); // object 
     if (hoveredItem) {
       hoveredItem.isHovered = true;
+      console.log(hoveredItem.isHovered)
     }
 
   }
@@ -23,6 +34,7 @@ export class SkillsComponent {
 
     if (hoveredItem) {
       hoveredItem.isHovered = false;
+      console.log(hoveredItem.isHovered)
     }
 
   }
